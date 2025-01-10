f4357fb59a745836b3f1a2419cda4bb94418ff0b
Maroš Antala <maros.antala@profesia.sk>
2025-01-10 07:30:07 +0100

    Removed forgotten comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <CssBaseline>
-            <App />
-        </CssBaseline>
+        <App />
     </StrictMode>
 );
